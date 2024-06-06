3ad034f613fe4e7c09f5c716a24042c93c81b17e
Takao Akaki <mongonta@gmail.com>
2024-06-06 23:58:16 +0900

    fix examples/Basic

--- examples/Basic/src/main.cpp
+++ examples/Basic/src/main.cpp
@@ -2,8 +2,15 @@
 #include <M5Unified.h>
 #include <SD.h>
 #include "Stackchan_ex_config.h"
+#include <Stackchan_servo.h>
+#include <Avatar.h>
 
-StackchanExConfig config;
+using namespace m5avatar;
+
+Avatar avatar;
+
+StackchanSERVO servo;
+StackchanExConfig system_config;
 
 void setup() {
   auto cfg = M5.config();
@@ -12,7 +19,23 @@ void setup() {
   M5.Log.setEnableColor(m5::log_target_serial, false);
   SD.begin(GPIO_NUM_4, SPI, 25000000);
   delay(2000);
-  config.loadConfig(SD, "/yaml/SC_BasicConfig.yaml");
+  system_config.loadConfig(SD, "/yaml/SC_BasicConfig.yaml");
+  
+  // servo
+  servo.begin(system_config.getServoInfo(AXIS_X)->pin, system_config.getServoInfo(AXIS_X)->start_degree,
+              system_config.getServoInfo(AXIS_X)->offset,
+              system_config.getServoInfo(AXIS_Y)->pin, system_config.getServoInfo(AXIS_Y)->start_degree,
+              system_config.getServoInfo(AXIS_Y)->offset,
+              (ServoType)system_config.getServoType());
+  delay(2000);
+  avatar.init();
+  
+  servo_interval_s* servo_interval = system_config.getServoInterval(AvatarMode::NORMAL); // ノーマルモード時のサーボインターバル情報を取得
+  servo_interval_s* servo_interval_sing = system_config.getServoInterval(AvatarMode::SINGING); // 歌っているときのサーボインターバル情報を取得
+
+  // wifi
+  wifi_s*     wifi_info = system_config.getWiFiSetting();
+  api_keys_s* api_key   = system_config.getAPISetting();
 
 }
 
